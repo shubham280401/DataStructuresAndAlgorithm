17b2ab79dd991451ab5fec0c00f91bb1db4063e4
shubham280401 <shubhambokde28@gmail.com>
2024-02-19 12:13:57 +0530

    DSA Q1 and 2

---
+++ 1.Arrays/1 Two sum.js
@@ -0,0 +1,18 @@
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+ */
+var twoSum = function (nums, target) {
+  let n = nums.length;
+  for (let i = 0; i <= n - 2; i++) {
+    for (let j = i + 1; j < n; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+console.log(twoSum([2, 3, 4, 5], 9));
