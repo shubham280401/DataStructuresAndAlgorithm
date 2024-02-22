8b9ec7ebadbf949db89fd2532ca5672887ca7432
shubham280401 <shubhambokde28@gmail.com>
2024-02-22 23:40:28 +0530

    151 reverse words in a string

---
+++ 2.Arrays  and two pointers/151 reverse words in a string.js
@@ -0,0 +1,61 @@
+/**
+ * 151. Reverse Words in a String
+Solved
+Medium
+Topics
+Companies
+Given an input string s, reverse the order of the words.
+
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated by a single space.
+
+Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+ 
+
+Example 1:
+
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+Example 2:
+
+Input: s = "  hello world  "
+Output: "world hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+Example 3:
+
+Input: s = "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s contains English letters (upper-case and lower-case), digits, and spaces ' '.
+There is at least one word in s.
+ */
+
+var reverseWords = function (s) {
+  let arr = s.split(" ").filter((e) => e !== "");
+  let i = 0;
+  let j = arr.length - 1;
+  while (i <= j) {
+    let temp = arr[j];
+    arr[j] = arr[i];
+    arr[i] = temp;
+    i++;
+    j--;
+  }
+  let newArr = arr.join(" ");
+  return newArr;
+};
+
+var reverseWords = function (s) {
+  let str = s.split(" ");
+  let output = "";
+  for (let i = str.length - 1; i >= 0; i--)
+    if (str[i]) output += (output ? " " : "") + str[i];
+  return output;
+};
